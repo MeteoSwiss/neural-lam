304073b8ceb3c74f64c6042c5f9bc7ba7ef8e80a
Simon Adamov <simon.adamov@meteoswiss.ch>
2023-10-30 13:30:28 +0100

    use latest checkpoint

--- slurm_eval.sh
+++ slurm_eval.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=normal
 #SBATCH --account=s83
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --time=23:59:00
 #SBATCH --output=logs/neurwp_eval.out
 #SBATCH --error=logs/neurwp_eval.err
@@ -19,5 +19,5 @@ NUM_GPUS=$(echo $SLURM_JOB_GPUS | tr ',' '\n' | wc -l)
 
 # Run the script with torchrun
 srun torchrun --nnodes=$SLURM_NNODES --nproc_per_node=$NUM_GPUS train_model.py \
-    --load "saved_models/graph_lam-4x64-10_22_03_53_18/min_val_loss.ckpt" \
+    --load "saved_models/graph_lam-4x64-10_29_14_34_54/min_val_loss.ckpt" \
     --dataset "cosmo" --eval="test" --subset_ds 1 --n_workers 31 --batch_size 4 --model "graph_lam"
--- slurm_train.sh
+++ slurm_train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=NeurWP16
+#SBATCH --job-name=NeurWP
 #SBATCH --partition=normal
 #SBATCH --account=s83
 #SBATCH --nodes=1
@@ -21,6 +21,6 @@ NUM_GPUS=$(echo $SLURM_JOB_GPUS | tr ',' '\n' | wc -l)
 
 # Run the script with torchrun
 srun torchrun --nnodes=$SLURM_NNODES --nproc_per_node=$NUM_GPUS train_model.py \
-    --dataset "cosmo" --subset_ds 0 --val_interval 27 --epochs 200 --n_workers 16 \
+    --dataset "cosmo" --subset_ds 0 --val_interval 42 --epochs 200 --n_workers 16 \
     --batch_size 1 --model "graph_lam" \
-    --load "saved_models/graph_lam-4x64-10_27_23_01_46/min_val_loss.ckpt"
+    --load "saved_models/graph_lam-4x64-10_29_14_34_54/min_val_loss.ckpt"

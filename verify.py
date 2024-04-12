eff6c5612cd62f61fdc5c2e7d1cad8c4f81d6bd1
Capucine Lechartre <capucine.lechartre@meteoswiss.ch>
2024-04-12 14:12:57 +0200

    remove useless file

--- verify.py
+++
@@ -1,4 +0,0 @@
-from neural_lam.vis import verify_inference
-
-
-verify_inference(file_path = "/users/clechart/clechart/neural-lam/wandb/run-20240411_140635-cux0r96n/files/results/inference/prediction_0.npy", feature_channel=24, save_path= "/users/clechart/clechart/neural-lam/neural_lam")
